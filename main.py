11251604ff9112c1032c9bc652350fd683853a1e
wellsbobby52 <wellsbobby52@gmail.com>
2025-04-23 12:56:19 -0600

    fastAPI backend template with get player api call

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+import httpx
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/nhl/player")
+async def get_player_data(player_id: int):
+    url = f"https://api-web.nhle.com/v1/player/{player_id}/landing?player_id={player_id}"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPError as e:
+            raise HTTPException(status_code=500, detail=str(e))
