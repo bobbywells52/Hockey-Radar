60cca0ca9cd062fcdf676a57ba43e0bf3382e0fb
wellsbobby52 <wellsbobby52@gmail.com>
2025-05-05 15:03:01 -0600

    tidy

--- backend/API/main.py
+++ backend/API/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.players import router as players_router
 from routers.teams import router as teams_router
 from mangum import Mangum
-import uvicorn
-app = FastAPI(debug=True)
+app = FastAPI()
 
 origins = ["*"]
 
@@ -22,30 +21,5 @@ app.include_router(teams_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-# @app.get("/nhl/player")
-# async def get_player_data(player_id: int):
-#     url = f"https://api-web.nhle.com/v1/player/{player_id}/landing?player_id={player_id}"
-#
-#     async with httpx.AsyncClient() as client:
-#         try:
-#             response = await client.get(url)
-#             response.raise_for_status()
-#             print("pulling data for player", player_id)
-#             return response.json()
-#         except httpx.HTTPError as e:
-#             raise HTTPException(status_code=500, detail=str(e))
-
-
-# @app.get("/nhl/team")
-# async def get_team_data(team_code: str):
-#     url = f"https://api-web.nhle.com/v1/roster/{team_code}/20242025"
-#     async with httpx.AsyncClient() as client:
-#         try:
-#             response = await client.get(url)
-#             response.raise_for_status()
-#             return response.json()
-#         except httpx.HTTPError as e:
-#             raise HTTPException(status_code=500, detail=str(e))
 
 handler = Mangum(app=app)
