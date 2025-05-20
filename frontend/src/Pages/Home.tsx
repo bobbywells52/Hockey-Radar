9f48d8ad5f0a02c474e5ab0195fc6991f7696cb1
wellsbobby52 <wellsbobby52@gmail.com>
2025-05-20 14:43:41 -0600

    add more star players on load

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -4,8 +4,9 @@ import ParentCard from "../components/ParentCard";
 import styles from "../App.module.css"
 import PlayerSearch from "../components/PlayerSearch"
 
-const shuffled = players.sort(() => 0.5 - Math.random());
-const slicedIds = [...shuffled.slice(0, 9), 8471214, 8477492, 8471675];
+const currentStars = shuffleArray([8471214, 8477492, 8471675, 8478402, 8477934, 8480069]);
+const activePlayers = players.sort(() => 0.5 - Math.random());
+const slicedIds = [...activePlayers.slice(0, 9), ...currentStars.slice(0,3)];
 const playerIds = shuffleArray(slicedIds);
 
 function Home() {
