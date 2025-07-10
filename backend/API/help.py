c49b3d76e88ccbb79c786a6888bf9da884bfb24a
wellsbobby52 <wellsbobby52@gmail.com>
2025-07-10 11:55:41 -0600

    update baseURL pointer for prod

--- backend/API/help.py
+++ backend/API/help.py
@@ -1,5 +1,19 @@
 import asyncio
-from backend.API.main import get_team_data
+from fastapi import APIRouter, HTTPException, Query
+import httpx
+
+router = APIRouter(prefix="/teams")
+
+@router.get("/{team_code}", status_code=200)
+async def get_team_data(team_code: str, season: str = Query(default='20242025')):
+    url = f"https://api-web.nhle.com/v1/roster/{team_code}/{season}"
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPError as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
 nhl_team_abbreviations = [ "ANA", "BOS", "BUF", "CGY", "CAR", "CHI", "COL",
     "CBJ", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", "NYI", "NYR",
@@ -53,10 +67,10 @@ async def fetch_all_rosters():
 player_dict = asyncio.run(fetch_all_rosters())
 list_of_dicts = [{key: value} for key, value in player_dict.items()]
 
-rows = []
-for d in list_of_dicts:
-    for k, v in d.items():
-        rows.append({"Player Name": k, "Player ID": v})
-        print(str(v) + ",")
+# rows = []
+# for d in list_of_dicts:
+#     for k, v in d.items():
+#         rows.append({"Player Name": k, "Player ID": v})
+#         print(str(v) + ",")
 
 
