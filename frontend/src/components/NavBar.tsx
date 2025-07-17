df76b29535ee27c60e6e5368cea6de287d99742f
wellsbobby52 <wellsbobby52@gmail.com>
2025-07-17 15:28:10 -0600

    third try is the charm

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,27 +7,27 @@ function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header class={styles.header}>
-            <div class={styles.header_content}>
+        <header className={styles.header}>
+            <div className={styles.header_content}>
                 <a href="#" className={styles.Logo}> HOCKEY RADAR</a>
                 <nav className={`${styles.nav} ${isOpen ? styles["nav--open"] : ""}`}>
-                    <ul class={styles.nav_list}>
-                        <li class={styles.nav_item}>
-                            <Link class={styles.nav_link}to="/">HOME</Link>
+                    <ul className={styles.nav_list}>
+                        <li className={styles.nav_item}>
+                            <Link className={styles.nav_link}to="/">HOME</Link>
                         </li>
                         <li className={styles.nav_item}>
-                            <Link class={styles.nav_link}to="/search">SEARCH</Link>
+                            <Link className={styles.nav_link}to="/search">SEARCH</Link>
                         </li>
-                        <li class={styles.nav_item}>
-                            <Link class={styles.nav_link}to="/about">ABOUT</Link>
+                        <li className={styles.nav_item}>
+                            <Link className={styles.nav_link}to="/about">ABOUT</Link>
                         </li>
                     </ul>
                 </nav>
 
                  <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
-                    <div class={styles.bar}></div>
-                    <div class={styles.bar}></div>
-                    <div class={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
                 </div>
             </div>
         </header>
