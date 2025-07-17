aaf2c293f0cdad78fef52820fe10fe8a762720ba
wellsbobby52 <wellsbobby52@gmail.com>
2025-07-17 12:22:24 -0600

    style and space navbar without mobile style

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <header class={styles.header}>
             <div class={styles.header_content}>
-                <a href="#" class={styles.logo}> HOCKEY RADAR</a>
+                <a href="#" class={styles.Logo}> HOCKEY RADAR</a>
                 <nav className={styles.nav}>
                     <ul class={styles.nav_list}>
                         <li class={styles.nav_item}>
