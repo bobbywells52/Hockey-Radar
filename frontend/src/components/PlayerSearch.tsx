23c63d08c687ae4f0d9b85e209606187ccb67765
wellsbobby52 <wellsbobby52@gmail.com>
2025-08-07 10:22:18 -0700

    add hard coded list of players to search component

--- frontend/src/components/PlayerSearch.tsx
+++ frontend/src/components/PlayerSearch.tsx
@@ -14,38 +14,23 @@ function PlayerSearch() {
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
-
-    const handleSearch = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setError("");
-
-        try {
-            if (query) {
-                navigate(`/player/${getPlayerIdByName(query)}`);
-            } else {
-                setError("Player not found.");
-            }
-        } catch (err) {
-            console.log(getPlayerIdByName(query))
-            setError("Error searching for player.");
-        }
-    };
-
     return (
         <div className={styles.searchContainer}>
             <div className={styles.contentWrapper}>
                 <h1 className={styles.title}>Search for an active NHL player</h1>
-                <form onSubmit={handleSearch} className={styles.searchForm}>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        placeholder="Enter full name or NHL API id..."
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                    <button type="submit" className={styles.button}>SEARCH</button>
-                </form>
-                {error && <div className={styles.error}>{error}</div>}
+                    <div className={styles.searchInputs}>
+                         <input
+                           className={styles.input}
+                           type="text"
+                           placeholder="Enter Player Name"
+                            />
+                            <button type="submit" className={styles.button}>SEARCH</button>
+                    </div>
+                    <div className={styles.searchResult}>
+                        {Players.map((value, key) => {
+                            return <div> {value.playerName} </div>})}
+                    </div>
+
             </div>
         </div>
     );
